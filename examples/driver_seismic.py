7f0e3e90280dc9bb9ac3ec1b2fe2e529df9b23b0
Carla Acosta <carla.acostasira@ucalgary.ca>
2024-02-17 08:09:00 -0700

    Updating function with the parameters and conditions

--- examples/driver_seismic.py
+++ examples/driver_seismic.py
@@ -20,39 +20,30 @@ import matplotlib.pyplot as plt
         
     """
 
-data = np.loadtxt(r"C:/Users/carla/Repos/goph420-w2024-lab01-stCA/examples/s_wave_data.txt")
+data = np.loadtxt(r"C:/Users/carla/Repos/goph420-w2024-lab01-stCA/examples/s_wave_data.txt") #Loading Data from .txt
 
 
 time = data[:,0] # First column of data (time)
 velocity = data[:,1] # Second column of data (velocity)
 
 max_abs_velocity = np.max(np.abs(velocity)) # Calculating the Max Absolute value of the velocity
-threshold_value =  0.005 * max_abs_velocity #Calculation the max velocity greater than 0.5%
-last_index = np.max(np.where(velocity > threshold_value))
+threshold_value =  0.005 * max_abs_velocity # Calculating the Max velocity greater than 0.5%
+last_index = np.max(np.where(velocity > threshold_value)) #Obtaining the index where that value is located in the velocity column
 
-# Get the corresponding time value
-T = time[last_index]
-velocity_2 = np.array()
-velocity_2 = velocity[:last_index +  1]
+T = time[last_index] # Getting the corresponding time value T within the criterion
+velocity_2 = velocity[:last_index +  1] # Creating a new veloccity array from the index 0 to last index +1
 
-time_2_list = [] 
-
-# Iterate over the time array
+time_2_list = [] # Creating a list that will store the values for the time
 for i in range(len(time)):
-    # If the current time value is less than or equal to T, append it to the list
-    if time[i] <= T:
+    if time[i] <= T: # Iterating over the column time and appending the numbers where time value is less than or equal to T
         time_2_list.append(time[i])
-    # Otherwise, break the loop because we've reached the end of the desired range
     else:
         break
 
-# Convert the list to a NumPy array
-time_2 = np.array(time_2_list)
+time_2 = np.array(time_2_list) # Converting the list to a NumPy array as time_2
 
-#plt.plot(time, velocity)
-#plt.xlabel('Time (s)')
-##plt.ylabel('Velocity (mm/s)')
-#plt.title('Raw Data Plot')
-#plt.show()
-
-print (time_2)
+plt.plot(time_2, velocity_2)
+plt.xlabel('Time (s)')
+plt.ylabel('Velocity (mm/s)')
+plt.title('Raw Data Plot')
+plt.show()
--- src/goph420_lab01/integration.py
+++ src/goph420_lab01/integration.py
@@ -1,3 +1,5 @@
+import numpy as np 
+
 def integrate_newton(x, f, alg): 
 """Compute numerical integration of discrete
    data using Newton-Cotes rules.
@@ -5,15 +7,13 @@ def integrate_newton(x, f, alg):
     Inputs
     ------
     x : array_like
-        Coordinates of sampling points given by the time [s]
     f : array_like
-        Values of the sampling points given by the velocity [mm/s]
     alg : str 
         Default value of 'trap' or 'simp' 
         
     Returns
     -------
-    I : float
+    integral : float
         Integral estimate
 
     Raises
@@ -24,3 +24,20 @@ def integrate_newton(x, f, alg):
     """
 
 x = 
+
+if not (isinstance(x, (list, np.ndarray)) and isinstance(f, (list, np.ndarray))):
+        raise ValueError("x and f must be array-like")
+    if np.shape(x) != np.shape(f):
+        raise ValueError("x and f must have the same shape")
+
+if alg not in ['trap', 'simp']:
+        raise ValueError(f"Invalid algorithm '{alg}'. Allowed values are 'trap' or 'simp'")
+    
+#Implement the trapezoid rule
+if alg == 'trap':
+    integral = np.trapz(f, x)
+# Implement Simpson's rule
+elif alg == 'simp':
+    integral = np.sum(f) * np.diff(x)[0] /  3  # Simplified version, actual Simpson's rule requires more work
+    
+    return integral
