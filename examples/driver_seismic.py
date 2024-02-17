64939cad4e872ddb2b7174e7930944e16a1adeb5
Carla Acosta <carla.acostasira@ucalgary.ca>
2024-02-16 22:40:28 -0700

    Generating the criterion for the T value

--- examples/driver_seismic.py
+++ examples/driver_seismic.py
@@ -12,24 +12,37 @@ import matplotlib.pyplot as plt
        
     Returns
     -------
-    numpy.ndarray, shape=(1, order+1)
-        The array of shape function values
+    Plot of the raw data
 
     Raises
     ------
     ValueError
-        If s cannot be converted to float.
-        If order is not in [1].
+        
     """
 
-data = np.loadtxt('C:\Users\carla\Repos\goph420-w2024-lab01-stCA/s_wave_data.txt', delimiter='\t', dtype=float)
+data = np.loadtxt(r"C:/Users/carla/Repos/goph420-w2024-lab01-stCA/examples/s_wave_data.txt")
 
 
-time = data[:,  0] # First column of data (time)
-velocity = data[:,  1] # Second column of data (velocity)
+time = data[:,0] # First column of data (time)
+velocity = data[:,1] # Second column of data (velocity)
 
-plt.plot(time, velocity)
-plt.xlabel('Time (s)')
-plt.ylabel('Velocity (mm/s)')
-plt.title('Raw Data Plot')
-plt.show()
+max_abs_velocity = np.max(np.abs(velocity)) # Calculating the Max Absolute value of the velocity
+threshold_value =  0.005 * max_abs_velocity #Calculation the max velocity greater than 0.5%
+last_index = np.max(np.where(velocity > threshold_value)) # Find the last index where velocity is greater than 0.5%
+
+T = time[last_index] 
+
+T_index = np.where(time == T)[0][0]
+
+# Create new arrays for time and velocity up to T
+time_up_to_T = time[:T_index+1]
+velocity_up_to_T = velocity[:T_index+1]
+
+#plt.plot(time, velocity)
+#plt.xlabel('Time (s)')
+##plt.ylabel('Velocity (mm/s)')
+#plt.title('Raw Data Plot')
+#plt.show()
+
+print (time_up_to_T)
+print (velocity_up_to_T)
--- src/goph420_lab01/integration.py
+++ src/goph420_lab01/integration.py
@@ -1,21 +1,26 @@
-def integrate_newton(x): 
+def integrate_newton(x, f, alg): 
 """Compute numerical integration of discrete
    data using Newton-Cotes rules.
 
     Inputs
     ------
     x : array_like
-        Coordinates of sampling points
+        Coordinates of sampling points given by the time [s]
     f : array_like
-        Values of thesampling points
-    alg : str
+        Values of the sampling points given by the velocity [mm/s]
+    alg : str 
+        Default value of 'trap' or 'simp' 
         
     Returns
     -------
-    float
+    I : float
+        Integral estimate
 
     Raises
     ------
     ValueError
-        If alg contain a str 
+        If alg contain a str different than 'trap' or 'simp'
+        If the dimensions of x and f are imcompatible
     """
+
+x = 
