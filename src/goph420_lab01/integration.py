Fixing the Simpsons Rule formula

src/goph420_lab01/integration.py
@@ -24,8 +24,6 @@ Compute numerical integration of discrete
         If alg contain a str different than 'trap' or 'simp'
     """
 
-
-
 if not (isinstance(x, (list, np.ndarray)) and isinstance(f, (list, np.ndarray))): # Comparing if x and f are array-like w/ same shape
     raise ValueError("x and f must be array-like")
 if np.shape(x) != np.shape(f):
@@ -39,18 +37,14 @@ if alg == 'trap':
     integral =  0.5 * (x[-1] - x[0]) * (f[0] +  2 * sum(f[1:N-1]) + f[-1])
     
 #Calculating the integration with Simpson's rule
-elif alg == 'simp': #How can I set up a condition that count the # of segments or points
-#    for i in a range(1:N)
-#        if i % 2 == 0:
-#            integral =  (x[-1] - x[0])/3 * (f[0] + 4 * sum(f[1:2*i-1] + 2 * f[2:2*i] + f[-1]))
-#        else:
-#            integral 
-
+elif alg == 'simp': 
+    for i in range(len(f))
+        if len(f) % 2 == 0: #If there are even number of points
+            integral =  (x[-1] - x[0])/3 * (f[0] + 4 * sum(f[1:2*i-1] + 2 * f[2:2*i] + f[-5])) + (x[-1] - x[-4])/(8*3) * (f[-4] + 3 * f[-3] + 3 * f[-2] + f[-1]) #Perform Simp 1/3 + 3/8 for the last 4 points 
+        else: 
+            integral = (x[-1] - x[0])/3 * (f[0] + 4 * sum(f[1:2*i-1] + 2 * f[2:2*i] + f[-1])) #Perform Simp 1/3  
 
     return integral
-
-
-
 
 def integrate_gauss(f, lims, npts=3):
     # Check if f is callable