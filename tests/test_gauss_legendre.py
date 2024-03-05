1c18529da639c3d5b3c2b7186306f2b07cfdb49c
Carla Acosta <carla.acostasira@ucalgary.ca>
2024-03-05 15:26:50 -0700

    Finished with the 9order polytest

--- tests/test_gauss_legendre.py
+++ tests/test_gauss_legendre.py
@@ -16,12 +16,27 @@ class TestIntegrateGaussPoly_1stOrder(unittest.TestCase):
     def test_integral_value_npts2(self):
         actual = integrate_gauss(self.f,self.x, npts=2)
         self.assertAlmostEqual(actual, self.expected) #continue
+        
+    def test_integral_value_npts3(self):
+        actual = integrate_gauss(self.f,self.x, npts=3)
+        self.assertAlmostEqual(actual, self.expected)
+        
+    def test_integral_value_npts4(self):
+        actual = integrate_gauss(self.f,self.x, npts=4)
+        self.assertAlmostEqual(actual, self.expected)
+    
+    def test_integral_value_npts5(self):
+        actual = integrate_gauss(self.f,self.x, npts=5)
+        self.assertAlmostEqual(actual, self.expected)
     
 class TestIntegrateGaussPoly_2ndOrder(unittest.TestCase):
     def setUp(self):
         self.x = [-2,2]
         self.f = lambda x: x**2 + 2*x - 1
-        self.expected = ((1/3) * self.x[-1]**3 + self.x[-1]**2 - self.x[-1] - ((1/3) * self.x[0]**3 + self.x[0]**2 - self.x[0]))    
+        self.expected = ((1/3) * self.x[-1]**3 
+                         + self.x[-1]**2 - self.x[-1] 
+                         - ((1/3) * self.x[0]**3 
+                            + self.x[0]**2 - self.x[0]))    
     
     def test_integral_value_npts2(self):
         actual = integrate_gauss(self.f,self.x, npts=2)
@@ -29,14 +44,24 @@ class TestIntegrateGaussPoly_2ndOrder(unittest.TestCase):
         
     def test_integral_value_npts3(self):
         actual = integrate_gauss(self.f,self.x, npts=3)
+        self.assertAlmostEqual(actual, self.expected)
+        
+    def test_integral_value_npts4(self):
+        actual = integrate_gauss(self.f,self.x, npts=4)
+        self.assertAlmostEqual(actual, self.expected)
+    
+    def test_integral_value_npts5(self):
+        actual = integrate_gauss(self.f,self.x, npts=5)
         self.assertAlmostEqual(actual, self.expected)
 
 class TestIntegrateGaussPoly_3rdOrder(unittest.TestCase):
     def setUp(self):
         self.x = [-2,2]
         self.f = lambda x: x**3 + x**2 + 2*x - 1
-        self.expected = ((1/4) * self.x[-1]**4 + (1/3) * self.x[-1]**3 + self.x[-1]**2 - self.x[-1] 
-                    - ((1/4) * self.x[0]**4 + (1/3) * self.x[0]**3 + self.x[0]**2 - self.x[0]))
+        self.expected = ((1/4) * self.x[-1]**4 + (1/3) * self.x[-1]**3 
+                         + self.x[-1]**2 - self.x[-1] 
+                    - ((1/4) * self.x[0]**4 + (1/3) * self.x[0]**3 
+                       + self.x[0]**2 - self.x[0]))
     
     def test_integral_value_npts2(self):
         actual = integrate_gauss(self.f,self.x, npts=2)
@@ -45,31 +70,132 @@ class TestIntegrateGaussPoly_3rdOrder(unittest.TestCase):
     def test_integral_value_npts3(self):
         actual = integrate_gauss(self.f,self.x, npts=3)
         self.assertAlmostEqual(actual, self.expected)
-
-# class TestIntegrateGaussPoly_4thOrder(unittest.TestCase):
-#     def setUp(self):
-#         self.x = [-2,2]
-#         self.f = lambda x: self.x **4 + self.x**3 + self.x**2 + 2*self.x - 1
+        
+    def test_integral_value_npts4(self):
+        actual = integrate_gauss(self.f,self.x, npts=4)
+        self.assertAlmostEqual(actual, self.expected)
     
-#     def test_integral_value(self):
-#         expected = ((1/5) * self.x[-1]**5 + (1/4) * self.x[-1]**4 + (1/3) * self.x[-1]**3 + self.x[-1]**2 - 1 
-#                     - ((1/5) * self.x[0]**5 + (1/4) * self.x[0]**4 + (1/3) * self.x[0]**3 + self.x[0]**2 - 1)) 
-#         for npts in range(1, 6): 
-#             actual = integrate_gauss(self.f,self.x, npts=npts)
-#             self.assertAlmostEqual(actual, expected)
+    def test_integral_value_npts5(self):
+        actual = integrate_gauss(self.f,self.x, npts=5)
+        self.assertAlmostEqual(actual, self.expected)
 
-# class TestIntegrateGaussPoly_5thOrder(unittest.TestCase):
-#     def setUp(self):
-#         self.x = np.linspace(-2, 2)
-#         self.f = self.x **5 + self.x **4 + self.x**3 + self.x**2 + 2*self.x - 1
+class TestIntegrateGaussPoly_4thOrder(unittest.TestCase):
+    def setUp(self):
+        self.x = [-2,2]
+        self.f = lambda x: x**4 + x**3 + x**2 + 2*x - 1
+        self.expected = ((1/5) * self.x[-1]**5 + (1/4) * self.x[-1]**4 
+                         + (1/3) * self.x[-1]**3 + self.x[-1]**2 - self.x[-1] 
+                    - ((1/5) * self.x[0]**5 + (1/4) * self.x[0]**4 
+                       + (1/3) * self.x[0]**3 + self.x[0]**2 - self.x[0]))
     
-#     def test_integral_value(self):
-#         expected = ((1/6) * self.x[-1]**6 + (1/5) * self.x[-1]**5 + (1/4) * self.x[-1]**4 + (1/3) * self.x[-1]**3 + self.x[-1]**2 - 1 
-#                     - ((1/6) * self.x[0]**6 + (1/5) * self.x[0]**5 + (1/4) * self.x[0]**4 + (1/3) * self.x[0]**3 + self.x[0]**2 - 1)) 
-#         for npts in range(1, 6): 
-#             actual = integrate_gauss(self.f,self.x, npts=npts)
-#             self.assertAlmostEqual(actual, expected)
+    def test_integral_value_npts3(self):
+        actual = integrate_gauss(self.f,self.x, npts=3)
+        self.assertAlmostEqual(actual, self.expected)
+        
+    def test_integral_value_npts4(self):
+        actual = integrate_gauss(self.f,self.x, npts=4)
+        self.assertAlmostEqual(actual, self.expected)
+    
+    def test_integral_value_npts5(self):
+        actual = integrate_gauss(self.f,self.x, npts=5)
+        self.assertAlmostEqual(actual, self.expected)
 
+class TestIntegrateGaussPoly_5thOrder(unittest.TestCase):
+    def setUp(self):
+        self.x = [-2,2]
+        self.f = lambda x: x**5 + x**4 + x**3 + x**2 + 2*x - 1
+        self.expected = ((1/6) * self.x[-1]**6 + (1/5) * self.x[-1]**5 
+                         + (1/4) * self.x[-1]**4 + (1/3) * self.x[-1]**3 
+                         + self.x[-1]**2 - self.x[-1] 
+                    - ((1/6) * self.x[0]**6 + (1/5) * self.x[0]**5 
+                       + (1/4) * self.x[0]**4 + (1/3) * self.x[0]**3 
+                       + self.x[0]**2 - self.x[0]))
+    
+    def test_integral_value_npts3(self):
+        actual = integrate_gauss(self.f,self.x, npts=3)
+        self.assertAlmostEqual(actual, self.expected)
+        
+    def test_integral_value_npts4(self):
+        actual = integrate_gauss(self.f,self.x, npts=4)
+        self.assertAlmostEqual(actual, self.expected)
+    
+    def test_integral_value_npts5(self):
+        actual = integrate_gauss(self.f,self.x, npts=5)
+        self.assertAlmostEqual(actual, self.expected)
+        
+class TestIntegrateGaussPoly_6thOrder(unittest.TestCase):
+    def setUp(self):
+        self.x = [-2,2]
+        self.f = lambda x: x**6 + x**5 + x **4 + x**3 + x**2 + 2*x - 1
+        self.expected = ((1/7) * self.x[-1]**7 + (1/6) * self.x[-1]**6 
+                         + (1/5) * self.x[-1]**5 + (1/4) * self.x[-1]**4 
+                         + (1/3) * self.x[-1]**3 + self.x[-1]**2 - self.x[-1] 
+                    - ((1/7) * self.x[0]**7 + (1/6) * self.x[0]**6 
+                       + (1/5) * self.x[0]**5 + (1/4) * self.x[0]**4 
+                       + (1/3) * self.x[0]**3 + self.x[0]**2 - self.x[0]))
+    
+    def test_integral_value_npts4(self):
+        actual = integrate_gauss(self.f,self.x, npts=4)
+        self.assertAlmostEqual(actual, self.expected)
+    
+    def test_integral_value_npts5(self):
+        actual = integrate_gauss(self.f,self.x, npts=5)
+        self.assertAlmostEqual(actual, self.expected)
+
+class TestIntegrateGaussPoly_7thOrder(unittest.TestCase):
+    def setUp(self):
+        self.x = [-2,2]
+        self.f = lambda x: x**7 + x**6 + x**5 + x **4 + x**3 + x**2 + 2*x - 1
+        self.expected = ((1/8) * self.x[-1]**8 + (1/7) * self.x[-1]**7 
+                         + (1/6) * self.x[-1]**6 + (1/5) * self.x[-1]**5 
+                         + (1/4) * self.x[-1]**4 + (1/3) * self.x[-1]**3 
+                         + self.x[-1]**2 - self.x[-1] 
+                    - ((1/8) * self.x[0]**8 + (1/7) * self.x[0]**7 
+                       + (1/6) * self.x[0]**6 + (1/5) * self.x[0]**5 
+                       + (1/4) * self.x[0]**4 + (1/3) * self.x[0]**3 
+                       + self.x[0]**2 - self.x[0]))
+    
+    def test_integral_value_npts4(self):
+        actual = integrate_gauss(self.f,self.x, npts=4)
+        self.assertAlmostEqual(actual, self.expected)
+    
+    def test_integral_value_npts5(self):
+        actual = integrate_gauss(self.f,self.x, npts=5)
+        self.assertAlmostEqual(actual, self.expected)
+
+class TestIntegrateGaussPoly_8thOrder(unittest.TestCase):
+    def setUp(self):
+        self.x = [-2,2]
+        self.f = lambda x: x**8 + x**7 + x**6 + x**5 + x **4 + x**3 + x**2 + 2*x - 1
+        self.expected = ((1/9) * self.x[-1]**9 + (1/8) * self.x[-1]**8 
+                         + (1/7) * self.x[-1]**7 + (1/6) * self.x[-1]**6 
+                         + (1/5) * self.x[-1]**5 + (1/4) * self.x[-1]**4 
+                         + (1/3) * self.x[-1]**3 + self.x[-1]**2 - self.x[-1] 
+                    - ((1/9) * self.x[0]**9 + (1/8) * self.x[0]**8 
+                       + (1/7) * self.x[0]**7 + (1/6) * self.x[0]**6 
+                       + (1/5) * self.x[0]**5 + (1/4) * self.x[0]**4 
+                       + (1/3) * self.x[0]**3 + self.x[0]**2 - self.x[0]))
+    
+    def test_integral_value_npts5(self):
+        actual = integrate_gauss(self.f,self.x, npts=5)
+        self.assertAlmostEqual(actual, self.expected)
+
+class TestIntegrateGaussPoly_9thOrder(unittest.TestCase):
+    def setUp(self):
+        self.x = [-2,2]
+        self.f = lambda x: x**9 + x**8 + x**7 + x**6 + x**5 + x **4 + x**3 + x**2 + 2*x - 1
+        self.expected = ((1/10) * self.x[-1]**10 + (1/9) * self.x[-1]**9 + (1/8) * self.x[-1]**8 
+                         + (1/7) * self.x[-1]**7 + (1/6) * self.x[-1]**6 
+                         + (1/5) * self.x[-1]**5 + (1/4) * self.x[-1]**4 
+                         + (1/3) * self.x[-1]**3 + self.x[-1]**2 - self.x[-1] 
+                    - ((1/10) * self.x[0]**10 + (1/9) * self.x[0]**9 + (1/8) * self.x[0]**8 
+                       + (1/7) * self.x[0]**7 + (1/6) * self.x[0]**6 
+                       + (1/5) * self.x[0]**5 + (1/4) * self.x[0]**4 
+                       + (1/3) * self.x[0]**3 + self.x[0]**2 - self.x[0]))
+    
+    def test_integral_value_npts5(self):
+        actual = integrate_gauss(self.f,self.x, npts=5)
+        self.assertAlmostEqual(actual, self.expected)
 
 # class TestIntegrateGaussNonPoly(unittest.TestCase):
 #     def test_function(self):
