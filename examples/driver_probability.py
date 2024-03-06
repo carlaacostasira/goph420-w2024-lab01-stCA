9adcd165c0633a996bf55affce909ae6148bff61
Carla Acosta <carla.acostasira@ucalgary.ca>
2024-03-05 21:46:32 -0700

    creating the probability function

--- examples/driver_probability.py
+++ examples/driver_probability.py
@@ -4,15 +4,13 @@ from scipy.stats import norm
 from goph420_lab01.integration import integrate_gauss
 
 # Define the mean and standard deviation
-mu1 = 1.5 # Mean of the distribution
-sigma1 = 0.5 # Standard deviation of the distribution
-
-x = np.linspace(mu1 - 3*sigma1, mu1 + 3*sigma1, 100)
-
-z = (x - mu1)/sigma1
-
+mu = 1.5 # Mean of the distribution
+sigma = 0.5 # Standard deviation of the distribution
+magnitude = 4.0
+z = (magnitude - mu)/sigma
+x = np.linspace(mu - 3*sigma, mu + 3*sigma, 100)
 # Calculate the PDF for the standard normal distribution at each x value
-pdf_values = norm.pdf(x, mu1, sigma1)
+pdf_values = norm.pdf(x, mu, sigma)
 
 # Plot the PDF
 plt.plot(x, pdf_values)
